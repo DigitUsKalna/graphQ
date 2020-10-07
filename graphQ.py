1ef53fce17157b9e9a0328e107ef01c24afe7839
d-kanti <dibyakanti22@gmail.com>
2020-10-08 01:17:17 +0530

    included __pycache__ into the gitignore and used basic functionality check. is working fine seperately.

--- graphQ.py
+++ graphQ.py
@@ -4,3 +4,13 @@ import  matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
+
+x = np.array([1,2,3,4,5])
+y = np.array([1,9,50,300,1500])
+
+popt, pcov = curve_fit(func,x,y)
+
+xFit = np.arange(0.0,5,0.01)
+
+plt.plot(xFit,func(xFit, *popt))
+plt.savefig('si.png')
