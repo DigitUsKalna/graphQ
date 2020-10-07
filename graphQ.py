4749f573950bbb07141439a98926e6fe6c91ce59
d-kanti <dibyakanti22@gmail.com>
2020-10-08 01:34:23 +0530

    included __pycache__ into the gitignore and used basic functionality check. is working fine seperately.

--- graphQ.py
+++ graphQ.py
@@ -3,14 +3,42 @@ import numpy as np
 import  matplotlib.pyplot as plt 
 from scipy.optimize import curve_fit
 
+def mean(list_provided):
+    return sum(list_provided)/len(list_provided)
 
 
-x = np.array([1,2,3,4,5])
-y = np.array([1,9,50,300,1500])
+def graph_fit(funcToUse, xData, yData, save_it = False, save_path = '.\img.png ', x_min=False, y_min = False, y_max=False, x_max=False,x_step = 0.01):
 
-popt, pcov = curve_fit(func,x,y)
+    if x_min and x_max and y_min and y_max :
+        pass
+    else:
+        m = min(xData)
+        M = max(xData)
+        avg = mean(xData)
+        diff = m-m
+        x_min = m - (diff/avg)
+        x_max = M + (diff/avg)
+        m = min(yData)
+        M = max(yData)
+        avg = mean(yData)
+        diff = m-m
+        y_min = m - (diff/avg)
+        y_max = M + (diff/avg)
 
-xFit = np.arange(0.0,5,0.01)
 
-plt.plot(xFit,func(xFit, *popt))
-plt.savefig('si.png')
+
+    popt, pcov = curve_fit(funcToUse,xData,yData)
+
+    xFit = np.arange(x_min,x_max,x_step)
+
+    plt.plot(xFit,func(xFit, *popt))
+    plt.plot(xData,yData,=r*)
+
+    if save_it:
+        plt.savafig(save_path)
+
+    
+    
+
+
+    
